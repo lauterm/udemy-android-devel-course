bdf63f761c7eec522aa26c85c9938c136482cc69
Matthew E. Lauterbach <lauterm@gmail.com>
2016-02-15 18:23:32 -0600

    End of lecture 29

---
+++ HelloWorld/learnjava/src/main/java/com/example/Demo.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Demo {
+    public static void main(String[] args) {
+        String testMessage = "This is another test.";
+        System.out.println(testMessage);
+
+        int firstNumber = 55;
+        int secondNumber = 5;
+
+        int result = (firstNumber + secondNumber) * 5;
+        System.out.println(result);
+
+        double myValue = 1235.67894;
+        double mySecondValue = 3.5594588587475;
+        System.out.println(myValue * mySecondValue);
+    }
+}
